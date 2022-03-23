8ac408b874384d102e80fb39512642db6165e061
Eder Rodrigues <ederpr@gmail.com>
2022-03-23 07:35:15 -0300

    Dto que recebera as informacoes do formulario de voto

--- src/main/java/br/com/estudo/votingApp/dto/RegistreVote.java
+++ src/main/java/br/com/estudo/votingApp/dto/RegistreVote.java
@@ -1,2 +1,13 @@
-package br.com.estudo.votingApp.dto;public class RegistreVote {
+package br.com.estudo.votingApp.dto;
+
+public class RegistreVote {
+    private String optionValue;
+
+    public String getOptionValue() {
+        return optionValue;
+    }
+
+    public void setOptionValue(String optionValue) {
+        this.optionValue = optionValue;
+    }
 }
