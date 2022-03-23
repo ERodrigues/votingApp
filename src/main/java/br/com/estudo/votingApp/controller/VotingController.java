23cb67eb4593ec08783f0fe8cd5dbf057e77a7d8
Eder Rodrigues <ederpr@gmail.com>
2022-03-23 07:34:21 -0300

    Controller para retornar a pagina de voto e receber a opcao de voto do usuario

--- src/main/java/br/com/estudo/votingApp/controller/VotingController.java
+++ src/main/java/br/com/estudo/votingApp/controller/VotingController.java
@@ -1,14 +1,24 @@
 package br.com.estudo.votingApp.controller;
 
+import br.com.estudo.votingApp.dto.RegistreVote;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/voting")
 public class VotingController {
 
-    @GetMapping("/voting")
+    @GetMapping("/")
     public String getOptions(Model model){
+        return "voting";
+    }
+
+    @PostMapping("/register")
+    public String register(RegistreVote registreVote){
+        System.out.println(registreVote.getOptionValue());
         return "voting";
     }
 }
