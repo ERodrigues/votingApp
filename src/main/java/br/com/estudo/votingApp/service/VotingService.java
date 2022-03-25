63ff009cf964d6fbf12d040819a4508840b22ad0
eder.rodrigues <ederpr@gmail.com>
2022-03-25 07:37:53 -0300

    Implementacao na classe de servico para envio de mensagem na fila

--- src/main/java/br/com/estudo/votingApp/service/VotingService.java
+++ src/main/java/br/com/estudo/votingApp/service/VotingService.java
@@ -3,14 +3,19 @@ package br.com.estudo.votingApp.service;
 import br.com.estudo.votingApp.dto.RegistreVote;
 import br.com.estudo.votingApp.model.Voting;
 import com.google.gson.Gson;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VotingService {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     public void register(RegistreVote registreVote) {
         Voting vote = new Voting(registreVote);
-        String jsonVote = getJsonVote(vote);
+        rabbitTemplate.convertAndSend("vote", getJsonVote(vote));
     }
 
     private String getJsonVote(Voting voting){
