388e8c31521fb1ef6252de56d1b3c0defbb88d4e
eder.rodrigues <ederpr@gmail.com>
2022-03-25 07:21:37 -0300

    dependencias do rabbitmq

---
+++ src/main/java/br/com/estudo/votingApp/config/ConectionRabbitMQ.java
@@ -0,0 +1,43 @@
+package br.com.estudo.votingApp.infra;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class ConectionRabbitMQ {
+    private final String QUEUENAME = "vote";
+    private final String EXCHANGENAME = "amq.direct";
+    private AmqpAdmin amqpAdmin;
+
+    public ConectionRabbitMQ(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue getQueue(){
+        return new Queue(QUEUENAME, true, false, false);
+    }
+
+    private DirectExchange getExchange(){
+        return new DirectExchange(EXCHANGENAME);
+    }
+
+    private Binding getBinding(Queue queue, DirectExchange directExchange){
+        return new Binding(queue.getName(), Binding.DestinationType.QUEUE, directExchange.getName(), queue.getName(), null);
+    }
+
+    @PostConstruct
+    private void createQueues(){
+        Queue queueVote = this.getQueue();
+        DirectExchange directExchange = this.getExchange();
+        Binding binding = this.getBinding(queueVote, directExchange);
+
+        amqpAdmin.declareQueue(queueVote);
+        amqpAdmin.declareExchange(directExchange);
+        amqpAdmin.declareBinding(binding);
+    }
+}
--- src/main/java/br/com/estudo/votingApp/infra/ConectionRabbitMQ.java
+++
@@ -1,8 +0,0 @@
-package br.com.estudo.votingApp.infra;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ConectionRabbitMQ {
-
-}
